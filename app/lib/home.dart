8cb62add4e5ed7ba95a38c78f10f289323a61c8d
Gabriella.E <63784697+bubleygum@users.noreply.github.com>
2023-05-10 14:32:44 +0700

    update

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -3,6 +3,16 @@ import 'package:apps/home_page.dart';
 import 'package:apps/theraphy.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_grid/responsive_grid.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  'assets/images/art1.jpg',
+  'assets/images/img1.jpg',
+  'assets/images/img2.jpg',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -146,6 +156,17 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         Expanded(
+          child: Container(
+              child: CarouselSlider(
+            options: CarouselOptions(
+              autoPlay: true,
+              aspectRatio: 2.0,
+              enlargeCenterPage: true,
+            ),
+            items: imageSliders,
+          )),
+        ),
+        Expanded(
           child: ListView(
             children: <Widget>[
               Card(
@@ -156,7 +177,7 @@ class HomeScreen extends StatelessWidget {
                     children: <Widget>[
                       Text('New Enrollment Discount'),
                       SizedBox(
-                        width: 100,
+                          width: 100,
                           child: ElevatedButton(
                               child: Text('Details'),
                               style: ElevatedButton.styleFrom(
@@ -174,7 +195,7 @@ class HomeScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: <Widget>[
                       SizedBox(
-                        width: 100,
+                          width: 100,
                           child: ElevatedButton(
                               child: Text('Tutorial'),
                               style: ElevatedButton.styleFrom(
@@ -192,7 +213,7 @@ class HomeScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: <Widget>[
                       SizedBox(
-                        width: 100,
+                          width: 100,
                           child: ElevatedButton(
                               child: Text('Tutorial'),
                               style: ElevatedButton.styleFrom(
@@ -210,7 +231,7 @@ class HomeScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: <Widget>[
                       SizedBox(
-                        width: 100,
+                          width: 100,
                           child: ElevatedButton(
                               child: Text('Tutorial'),
                               style: ElevatedButton.styleFrom(
@@ -228,3 +249,65 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+class ImageSliderDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Image slider demo')),
+      body: Container(
+          child: CarouselSlider(
+        options: CarouselOptions(),
+        items: imgList
+            .map((item) => Container(
+                  child: Center(
+                      child:
+                          Image.network(item, fit: BoxFit.cover, width: 1000)),
+                ))
+            .toList(),
+      )),
+    );
+  }
+}
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Text(
+                          'No. ${imgList.indexOf(item)} image',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
