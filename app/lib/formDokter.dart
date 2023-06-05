566ae205bc98cfa52cb5ca7be9b45ab0e0278889
Gabriella.E <63784697+bubleygum@users.noreply.github.com>
2023-06-05 15:32:37 +0700

    update

---
+++ app/lib/formDokter.dart
@@ -0,0 +1,313 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:apps/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MySignUpPage extends StatelessWidget {
+  const MySignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(title: Text('Sign Up')),
+      body: SignUpForm(),
+    );
+  }
+}
+
+class SignUpForm extends StatelessWidget {
+  SignUpForm({super.key});
+  TextEditingController _emailController  = TextEditingController();
+  TextEditingController _usernameController  = TextEditingController();
+  TextEditingController _passwordController  = TextEditingController();
+  TextEditingController _phoneNumberController  = TextEditingController();
+  TextEditingController pengalaman  = TextEditingController();
+  TextEditingController jabatan  = TextEditingController();
+  // show the password or not
+  bool _isObscure = true;
+
+  bool validatePhoneNumber(String phone) {
+    // Use a regular expression to check if the phone number is valid
+    final RegExp phoneRegex = RegExp(r'^[0-9]{10}$');
+    return phoneRegex.hasMatch(phone);
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    final phoneCont = TextEditingController();
+    final passCont = TextEditingController();
+    // regular expression to check if string
+    RegExp pass_valid = RegExp("(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)");
+    //A function that validate user entered password
+    bool validatePassword(String pass) {
+      String _password = pass.trim();
+      if (pass_valid.hasMatch(_password)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    var column = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          width: 300,
+          height: 30,
+          child: TextFormField(
+            controller: _usernameController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Username harus di isi';
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.person),
+              hintText: 'Username',
+              errorStyle: TextStyle(height: 0.5, fontSize: 0),
+              contentPadding:
+                  EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10),
+              border: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 0.3)),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 15),
+        SizedBox(
+          width: 300,
+          height: 30,
+          child: TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.mail),
+              hintText: 'Email address',
+              errorStyle: TextStyle(height: 0.5, fontSize: 0),
+              errorMaxLines: 2,
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              contentPadding:
+                  EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10),
+              border: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 0.3)),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'email harus di isi';
+              }
+              return null;
+            },
+          ),
+        ),
+        SizedBox(height: 15),
+        SizedBox(
+          width: 300,
+          height: 30,
+          child: TextFormField(
+            controller: _passwordController,
+            obscureText: _isObscure,
+            decoration: InputDecoration(
+              icon: Icon(Icons.key),
+              hintText: 'Password',
+              errorStyle: TextStyle(height: 0.5, fontSize: 0),
+              errorMaxLines: 2,
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              contentPadding:
+                  EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10),
+              border: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 0.3)),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty ) {
+                return 'Password harus di isi';
+              }
+              return null;
+            },
+          ),
+        ),
+        SizedBox(height: 15),
+        SizedBox(
+          width: 300,
+          height: 30,
+          child: TextFormField(
+            obscureText: _isObscure,
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.key),
+              hintText: 'Re-type Password',
+              errorStyle: TextStyle(height: 0.5, fontSize: 0),
+              errorMaxLines: 2,
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              contentPadding:
+                  EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10),
+              border: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 0.3)),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty || value != _passwordController.text.toString()) {
+                return 'Password harus di isi';
+              }
+              return null;
+            },
+          ),
+        ),
+        SizedBox(height: 15),
+        SizedBox(
+          width: 300,
+          height: 30,
+          child: TextFormField(
+            controller: _phoneNumberController,
+            decoration: const InputDecoration(
+              icon: Icon(FontAwesomeIcons.phone),
+              hintText: 'Phone number',
+              errorStyle: TextStyle(height: 0.5, fontSize: 0),
+              errorMaxLines: 2,
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              contentPadding:
+                  EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10),
+              border: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 0.3)),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Phone number harus di isi';
+              }
+              return null;
+            },
+          ),
+        ),
+        SizedBox(height: 15),
+        SizedBox(
+          width: 300,
+          height: 30,
+          child: TextFormField(
+            controller: jabatan,
+            decoration: const InputDecoration(
+              icon: FaIcon(FontAwesomeIcons.rankingStar),
+              hintText: 'Jabatan saat ini: ',
+              errorStyle: TextStyle(height: 0.5, fontSize: 0),
+              errorMaxLines: 2,
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              contentPadding:
+                  EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10),
+              border: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 0.3)),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Jabatan harus di isi';
+              }
+              return null;
+            },
+          ),
+        ),
+        SizedBox(height: 15),
+        SizedBox(
+          width: 300,
+          height: 30,
+          child: TextFormField(
+            controller: pengalaman,
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.handshake),
+              hintText: 'Pengalaman... tahun',
+              errorStyle: TextStyle(height: 0.5, fontSize: 0),
+              errorMaxLines: 2,
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              contentPadding:
+                  EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 10),
+              border: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: Color.fromRGBO(0, 0, 0, 0.3)),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Pengalaman';
+              }
+              return null;
+            },
+          ),
+        ),
+        SizedBox(height: 20),
+        SizedBox(
+            width: 250,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromRGBO(0, 74, 173, 1),
+              ),
+              child: const Text('Sign Up'),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: _emailController.text, 
+                    password: _passwordController.text).then((value){
+                      final UserUID = value.user?.uid.toString();
+                      createUser(userUID: UserUID);
+                      
+                      Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(builder: (context) => MyLogInPage()), 
+                        (Route<dynamic> route) => false);
+                      }).onError((error, stackTrace) {
+                      print("error ${error.toString()}");
+                    });
+                }
+                
+                      
+              },
+            )),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Already Have Account?",
+                  style: TextStyle(color: Color.fromRGBO(30, 100, 192, 1))),
+                GestureDetector(
+                  onTap: (){
+                    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => MyLogInPage()), (Route<dynamic> route) => false);
+                  },
+                  child: const Text(" Sign In",
+                  style: TextStyle(color: Color.fromRGBO(0, 74, 173, 1), fontWeight: FontWeight.bold)),
+                )
+
+              ],
+
+            )
+      ],
+    );
+    return Form(
+      key: _formKey,
+      child: Center(
+        child: column,
+      ),
+    );
+  }
+  
+  Future createUser({String? userUID}) async{
+    final docDokter = FirebaseFirestore.instance.collection('doctorList').doc(userUID);
+    final json = {
+      'jabatan': jabatan.text.toString(),
+      'pengalaman': pengalaman.text.toString()
+    };
+    //create document and write to DB
+    await docDokter
+      .set(json)
+      .whenComplete(() => print("added to database"))
+      .catchError((e) => print(e));
+  }
+}
+
+
